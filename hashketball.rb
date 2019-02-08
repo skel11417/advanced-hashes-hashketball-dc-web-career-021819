d92e9e42341df8068c97350c7ccfa01d07906110
Sean Kelly <shonkelli@gmail.com>
2019-02-08 23:06:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,6 +31,25 @@ end
 
 def shoe_size(player_name)
   player_stats(player_name)[:shoe]
+  game_hash.each do |team, team_info|
+    players = team_info[:players]
+    players.each do |player, stats|
+      if player_name == player
+        return stats[:points]
+      end
+    end
+  end
+end
+
+def shoe_size(player)
+  game_hash.each do |team, team_info|
+    players = team_info[:players]
+    players.each do |player, stats|
+      if player_name == player
+        return stats[:shoe]
+      end
+    end
+  end
 end
 
 def team_colors(team_name)
@@ -49,17 +68,30 @@ def team_names
   output
 end
 
+# def player_numbers(team_name)
+#   output = []
+#   game_hash.each do |team, team_info|
+#     if team_info[:team_name] == team_name
+#       team_info[:players].each do |player_name, stats|
+#         output << stats[:number]
+#       end
+#     end
+#   end 
+#   output
+# end
+
 def player_numbers(team_name)
-  output = []
-  game_hash.each do |team, team_info|
-    if team_info[:team_name] == team_name
-      team_info[:players].each do |player_name, stats|
-        output << stats[:number]
-      end
-    end
-  end 
-  output
-end 
+  team_info = game
+end
+
+def get_team_info(team_name)
+  team_info = game_hash.values
+  team_info.find {|team_hash| team_hash[:team_name] == team_name}
+end
+
+print get_team_info("Brooklyn Nets")
+
+
 
 def player_stats(player_name)
   game_hash.each do |team, team_info|
@@ -84,4 +116,21 @@ def big_shoe_rebounds
     end
   end  
   output
+  # output = Array.new
+  game_hash.each do |team, team_name|
+    return team_name[:team_name]
+  end
+  # output
+end
+
+def player_numbers(team)
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == team_name
+      
+    end
+  end  
+end 
+
+def player_stats(player)
+  
 end
